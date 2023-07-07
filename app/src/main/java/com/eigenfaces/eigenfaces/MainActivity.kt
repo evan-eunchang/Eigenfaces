4b7655a98d374a4ec8a6df51d52c4a420f9c9861
Evan Lee <evanel2@illinois.edu>
2023-07-07 14:14:24 -0500

    File is now readable

---
+++ app/src/main/java/com/eigenfaces/eigenfaces/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.eigenfaces.eigenfaces
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Scanner
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var array = Array(10304) {FloatArray(400)}
+        val istream = resources.openRawResource(R.raw.vfaces)
+        val scanner = Scanner(istream)
+        var line: String
+        val start = System.currentTimeMillis()
+        var row = 0
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine()
+            val list = line.split(',')
+            for (i in list.indices) {
+                array[row][i] = list[i].toFloat()
+            }
+            row++
+        }
+        Log.i("STREAM_TAG", (System.currentTimeMillis() - start).toString())
+        istream.close()
+        scanner.close()
+    }
+}
