31a9fe869991d3a94fcd74fdf2da3a5ffaaf9104
Evan Lee <evanel2@illinois.edu>
2023-07-14 22:42:35 -0500

    -fixed database errors -cleaned up UI -ran small scale tests -done

--- app/src/main/java/com/eigenfaces/eigenfaces/MainActivity.kt
+++ app/src/main/java/com/eigenfaces/eigenfaces/MainActivity.kt
@@ -2,9 +2,7 @@ package com.eigenfaces.eigenfaces
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.eigenfaces.eigenfaces.databinding.ActivityMainBinding
 import com.eigenfaces.eigenfaces.db.PortraitDao
 import com.eigenfaces.eigenfaces.db.PortraitDatabase
@@ -12,10 +10,9 @@ import com.eigenfaces.eigenfaces.db.PortraitDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    //private lateinit var portraitViewModel: PortraitViewModel
-    //var dao : PortraitDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("ROOM_TAG", "MainActivity was created!")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
